e14c0e28b42bb4159fca45bf77f22828ff2a823b
Malabeadman <deangelo.lee19@gmail.com>
2021-12-20 19:40:40 -0600

    Boy shooter game just a boy shooting

---
+++ app/src/main/java/com/example/waterfight/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.waterfight;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setContentView(new waterfight(this));
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+}
